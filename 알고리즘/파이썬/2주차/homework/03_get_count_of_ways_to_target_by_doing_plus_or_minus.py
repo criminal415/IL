6a1b2dddd15095ae6bae5a7aa970d5b5b6dde4be
criminal415 <denny415@naver.com>
2021-12-30 23:56:57 +0900

    [feat] 2주차 homework

---
+++ 알고리즘/파이썬/2주차/homework/03_get_count_of_ways_to_target_by_doing_plus_or_minus.py
@@ -0,0 +1,20 @@
+numbers = [2, 3, 1]
+target_number = 0
+result_count = 0  # target 을 달성할 수 있는 모든 방법의 수를 담기 위한 변수
+
+
+def get_count_of_ways_to_target_by_doing_plus_or_minus(array, target, current_index, current_sum):
+    if current_index == len(array):  
+        if current_sum == target:
+            global result_count
+            result_count += 1  
+        return
+    get_count_of_ways_to_target_by_doing_plus_or_minus(array, target, current_index + 1,
+                                                       current_sum + array[current_index])
+    get_count_of_ways_to_target_by_doing_plus_or_minus(array, target, current_index + 1,
+                                                       current_sum - array[current_index])
+
+
+get_count_of_ways_to_target_by_doing_plus_or_minus(numbers, target_number, 0, 0)
+
+print(result_count)  # 2가 반환됩니다!
