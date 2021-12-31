706c42f60dd6be52d499cc6199e396286e5bd96f
criminal415 <denny415@naver.com>
2022-01-01 00:15:50 +0900

    [edit] 삽입 정렬 수정

--- 알고리즘/파이썬/3주차/03_insertion_sort.py
+++ 알고리즘/파이썬/3주차/03_insertion_sort.py
@@ -3,7 +3,7 @@ input = [4, 6, 2, 9, 1]
 
 def insertion_sort(array):
     n = len(array)
-    for i in range(1, 5):
+    for i in range(n):
         for j in range(i):
             if array[i - j - 1] > array[i - j]:
                 array[i - j - 1], array[i - j] = array[i - j], array[i - j - 1]
