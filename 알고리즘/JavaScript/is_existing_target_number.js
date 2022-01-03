9404ecb843a76581e0a0f2b75f90adf492692b01
criminal415 <denny415@naver.com>
2022-01-03 20:10:35 +0900

    [edit] binary search 추가

--- 알고리즘/JavaScript/is_existing_target_number.js
+++ 알고리즘/JavaScript/is_existing_target_number.js
@@ -13,3 +13,25 @@ function is_existing_target_number(target, array) {
 }
 
 console.log(is_existing_target_number(finding_target, finding_numbers));
+
+//binary
+function is_existing_target_number_binary(target, array) {
+  let cur_index_min = 0;
+  let cur_index_max = array.length - 1;
+  let cur_index_guess = Math.floor((cur_index_min + cur_index_max) / 2);
+
+  while (cur_index_min <= cur_index_max) {
+    if (array[cur_index_guess] == target) {
+      return true;
+    } else if (array[cur_index_guess] < target) {
+      cur_index_min = cur_index_guess + 1;
+      cur_index_guess = Math.floor((cur_index_min + cur_index_max) / 2);
+    } else {
+      cur_index_max = cur_index_guess - 1;
+      cur_index_guess = Math.floor((cur_index_min + cur_index_max) / 2);
+    }
+  }
+  return false;
+}
+
+console.log(is_existing_target_number_binary(finding_target, finding_numbers));
