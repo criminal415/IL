dbc5ecb42b9259074a039085aabef0b49cdfa4ce
criminal415 <denny415@naver.com>
2022-01-03 15:56:28 +0900

    [edit] linkedlist node삽입 추가

--- 알고리즘/JavaScript/print_all_linked_list.js
+++ 알고리즘/JavaScript/print_all_linked_list.js
@@ -36,9 +36,34 @@ class LinkedList {
       cur = cur.next;
     }
   }
+  get_node(index) {
+    let node = this.head;
+    let count = 0;
+    while (count < index) {
+      node = node.next;
+      count += 1;
+    }
+    return node;
+  }
+
+  add_node(index, value) {
+    let new_node = new Node(value);
+    if (index == 0) {
+      new_node.next = this.head;
+      this.head = new_node;
+      return;
+    }
+    let node = this.get_node(index - 1);
+    let next_node = node.next;
+    node.next = new_node;
+    new_node.next = next_node;
+  }
 }
 
 let linked_list = new LinkedList(3);
 linked_list.append(4);
 linked_list.append(5);
 linked_list.print_all();
+linked_list.add_node(1, 7);
+linked_list.print_all();
+console.log(linked_list.get_node(0));
